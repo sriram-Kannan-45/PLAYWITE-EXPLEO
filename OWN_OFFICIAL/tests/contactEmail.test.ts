1f7ae6ed680f0e71a21ac328aa26fc2a52fab62e
sriram-Kannan-45 <wavene20@gmail.com>
2026-06-24 20:09:59 +0530

    lms

--- OWN_OFFICIAL/tests/contactEmail.test.ts
+++ OWN_OFFICIAL/tests/contactEmail.test.ts
@@ -1,1 +1,20 @@
-i
+import { test, expect } from '@playwright/test'
+
+test ('emailContact', async ({page}) =>
+{
+    await page.goto(process.env.BASE_URL!)
+
+    await page.click('(//a[text()="Contact"])[1]')
+
+    await page.locator('input[name="name"]').fill('sriram')
+
+    await page.locator('input[name="email"]').fill('titooram123@gmail.com')
+
+    await page.locator('select[name="projectType"]').selectOption('other')
+
+    await page.locator('textarea[name="message"]').fill('need a website')
+
+    await page.locator('button[type="submit"]').click()
+
+    await expect(page.locator('[role="status"]')).toBeVisible({ timeout: 10000 })
+})
